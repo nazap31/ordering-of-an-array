cc981b8849266d5749d5a4977cc8bd59ea4da2c4
nazap31 <you@example.com>
2022-07-26 11:40:41 -0600

    ordering of an array

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("\nOrdenamiento de un Array!\n\n");
+
+Console.WriteLine("\nrandom Array!\n");
+int[] array = new int [5000];
+int randomNumber;
+Random randomNumbers = new Random();
+
+for (int i = 0; i < 1000; i++)
+{
+    int numbers =  randomNumbers.Next(0, 1000);
+    array[i] = numbers;
+    randomNumber = array[i];
+    Console.Write(randomNumber + " ");
+}
+Console.WriteLine("\n\nArray in order!\n\n");
+Array.Sort(array);
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
